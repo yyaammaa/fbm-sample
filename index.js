2f09195efc7d3a207ec185754d38ce9c2bf3fd25
yamabit <yama@supership.jp>
2016-04-14 12:24:18 +0900

    misc

--- index.js
+++ index.js
@@ -24,15 +24,16 @@ app.get('/webhook/', (req, res) => {
 });
 
 app.post('/webhook/', (req, res) => {
-  let messaging_events = req.body.entry[0].messaging;
-  for (let i = 0; i < messaging_events.length; i++) {
-    let event = req.body.entry[0].messaging[i];
-    let sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      // Handle a text message from this sender
-    }
-  }
+  console.log(req);
+  //let messaging_events = req.body.entry[0].messaging;
+  //for (let i = 0; i < messaging_events.length; i++) {
+  //  let event = req.body.entry[0].messaging[i];
+  //  let sender = event.sender.id;
+  //  if (event.message && event.message.text) {
+  //    text = event.message.text;
+  //    // Handle a text message from this sender
+  //  }
+  //}
   res.sendStatus(200);
 });
 
