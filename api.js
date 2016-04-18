1d0e81446ab4f4f1d3dd100bc9dbaaaa2fff1523
yamabit <yama@supership.jp>
2016-04-18 11:14:25 +0900

    buttons

--- api.js
+++ api.js
@@ -65,15 +65,32 @@ let api = {
             "title": "First card",
             "subtitle": "Element #1 of an hscroll",
             "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-            "buttons": [{
-              "type": "web_url",
-              "url": "https://www.messenger.com/",
-              "title": "Web url"
-            }, {
-              "type": "postback",
-              "title": "Postback",
-              "payload": "Payload for first element in a generic bubble",
-            }],
+            "buttons": [
+              {
+                "type": "web_url",
+                "url": "https://www.messenger.com/",
+                "title": "Web url"
+              }, {
+                "type": "postback",
+                "title": "Postback",
+                "payload": "Payload for first element in a generic bubble",
+              },
+              {
+                "type": "web_url2",
+                "url": "https://www.messenger.com/",
+                "title": "Web url"
+              },
+              {
+                "type": "web_url3",
+                "url": "https://www.messenger.com/",
+                "title": "Web url"
+              },
+              {
+                "type": "web_url4",
+                "url": "https://www.messenger.com/",
+                "title": "Web url"
+              }
+            ]
           }, {
             "title": "Second card",
             "subtitle": "Element #2 of an hscroll",
