f4a7175e3e32033cb8ad24a4e95801478c262e04
yamabit <yama@supership.jp>
2016-04-23 10:31:43 +0900

    1

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.get('/webhook/', (req, res) => {
 
 app.post('/webhook/', (req, res) => {
   console.log(JSON.stringify(req.body));
+  res.sendStatus(200);
 
   // TODO: entry[0]以外も処理する必要がある?
   const messagingEvents = req.body.entry[0].messaging;
@@ -52,16 +53,7 @@ app.post('/webhook/', (req, res) => {
 
     if (event.message && event.message.text) {
       let text = event.message.text;
-      // Handle a text message from this sender
       console.log('Receive text: text = ' + text + ', sender = ' + sender);
-
-      //if (text === 'Generic') {
-      //  sendGenericMessage(sender);
-      //} else if (text === 'who') {
-      //  getUserInfo(sender);
-      //} else {
-      //  sendTopMessage(sender);
-      //}
 
       nanapiSearch(text, (error, response, body) => {
         if (error) {
@@ -69,30 +61,16 @@ app.post('/webhook/', (req, res) => {
           //} else if (response.body.error) {
           //  console.log('Error: ', response.body.error);
         } else {
-          const json = JSON.stringify(response.body);
-          console.log('Success: ', json);
-//    console.log('Success: ', response.body);
+          console.log('Success: ', JSON.stringify(response.body));
 
           const hits = body.hits.hits;
           sendGeneric(sender, hits);
-
-          //let tmp = null;
-          //_.each(hits, hit => {
-          //  const title = hit._source.title;
-          //  const url = hit._source.url;
-          //  const imageUrl = hit._source.image_url;
-          //  const desc = hit._source.desc;
-          //  console.log(title + ', ' + url + ', ' + imageUrl + ', ' + desc);
-          //
-          //  if (!tmp) tmp = title;
-          //});
-          //sendTextMessage(sender, tmp || '(not found)');
 
         }
       });
     }
   }
-  res.sendStatus(200);
+  //res.sendStatus(200);
 });
 
 app.listen(app.get('port'), (err) => {
@@ -116,31 +94,7 @@ const getUserInfo = (sender) => {
   });
 };
 
-let sendTopMessage = (sender) => {
-  api.sendTopMessage(sender, (error, response, body) => {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    } else {
-      console.log('Success: ', response.body);
-    }
-  });
-};
-
-let sendGenericMessage = (sender) => {
-  api.sendGenericMessage(sender, (error, response, body) => {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    } else {
-      console.log('Success: ', response.body);
-    }
-  });
-};
-
-let sendTextMessage = (sender, text) => {
+const sendTextMessage = (sender, text) => {
   api.sendTextMessage(sender, text, (error, response, body) => {
     if (error) {
       console.log('Error sending message: ', error);
