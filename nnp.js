93729bd2d819b9bd4cf42f90fe0d8241fa1f3d79
yamabit <yama@supership.jp>
2016-04-26 13:06:13 +0900

    radix

--- nnp.js
+++ nnp.js
@@ -94,10 +94,10 @@ const handlePostback = (sender, postback) => {
 
   const search = PAYLOAD.search + '?';
   if (payload.includes(search)) {
-    const qs = queryParser.parse(payload.substr(search.length), 10);
+    const qs = queryParser.parse(payload.substr(search.length));
     if (qs.query) {
       const offsetString = qs.offset || '0';
-      handleSearch(sender, qs.query, parseInt(offsetString));
+      handleSearch(sender, qs.query, parseInt(offsetString), 10);
     } else {
       console.log('handlePostback: Error: no query');
     }
